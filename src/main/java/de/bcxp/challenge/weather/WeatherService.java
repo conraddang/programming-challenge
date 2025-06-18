39232eac03562be4ebec490214119daee491e0e7
Conrad Dang <conrad.dang@tum.de>
2025-06-18 14:09:23 +0200

    implemented WeatherService class

--- src/main/java/de/bcxp/challenge/weather/WeatherService.java
+++ src/main/java/de/bcxp/challenge/weather/WeatherService.java
@@ -1,4 +1,12 @@
 package de.bcxp.challenge.weather;
 
+import java.util.Comparator;
+import java.util.List;
+
 public class WeatherService {
+    public WeatherData findDayWithSmallestTempSpread(List<WeatherData> dataEntries) {
+        return dataEntries.stream()
+                .min(Comparator.comparingInt(WeatherData::getTempSpread))
+                .orElse(null);
+    }
 }
