6a0275703593e8fe8e7691a66d02cb69e59c35b0
Conrad Dang <conrad.dang@tum.de>
2025-06-18 15:21:20 +0200

    added extractFields() method

--- src/main/java/de/bcxp/challenge/common/DataLoader.java
+++ src/main/java/de/bcxp/challenge/common/DataLoader.java
@@ -1,8 +1,13 @@
 package de.bcxp.challenge.common;
 
+import de.bcxp.challenge.weather.WeatherData;
+
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 public interface DataLoader<T> {
         List<T> loadData(String filePath) throws Exception;
+        Optional<T> extractFields(Map<String, String> row);
 }
 
