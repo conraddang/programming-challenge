1bafff0bf4f8361143916020746657416d1dbf9a
Conrad Dang <conrad.dang@tum.de>
2025-06-18 15:38:10 +0200

    added CountryTask class

--- src/main/java/de/bcxp/challenge/country/CountryTask.java
+++ src/main/java/de/bcxp/challenge/country/CountryTask.java
@@ -1,4 +1,30 @@
 package de.bcxp.challenge.country;
 
-public class CountryTask {
+import de.bcxp.challenge.common.Task;
+import de.bcxp.challenge.weather.WeatherData;
+import de.bcxp.challenge.weather.WeatherDataLoader;
+import de.bcxp.challenge.weather.WeatherService;
+
+import java.util.List;
+
+public class CountryTask implements Task {
+    private final static String RESOURCE_PATH = "de/bcxp/challenge/countries.csv";
+    private final CountryDataLoader countyDataLoader = new CountryDataLoader(';');
+    private final CountryService countryService = new CountryService();
+
+    @Override
+    public String execute() throws Exception {
+        List<CountryData> dataEntries =  countyDataLoader.loadData(RESOURCE_PATH);
+        for (CountryData data : dataEntries) {
+            System.out.println("Name: " + data.getName() + ", Population: " + data.getPopulation() + ", Area: " + data.getArea() + ", Population Density: " + data.getPopulationDensity());
+        }
+        return countryService.findCountryWithHighestPopulationDensity(dataEntries).getName();
+    }
+
+
+
+
+
+
+
 }
