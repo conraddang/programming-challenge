69a868c683d0a1d583859165231d4370f323f6f8
Conrad Dang <conrad.dang@tum.de>
2025-06-18 15:18:24 +0200

    added CSVDataLoader class

---
+++ src/main/java/de/bcxp/challenge/common/CSVDataLoader.java
@@ -0,0 +1,38 @@
+package de.bcxp.challenge.common;
+
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReaderHeaderAware;
+import com.opencsv.CSVReaderHeaderAwareBuilder;
+import de.bcxp.challenge.weather.WeatherData;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class CSVDataLoader {
+    private final char separator;
+
+    public CSVDataLoader(char separator) {
+        this.separator = separator;
+    }
+
+    public List<Map<String, String>> parse(String resourcePath) {
+        List<Map<String, String>> allRows = new ArrayList<>();
+        CSVParser parser = new CSVParserBuilder().withSeparator(separator).build();
+        InputStream input = getClass().getClassLoader().getResourceAsStream(resourcePath);
+
+        try (CSVReaderHeaderAware reader = new CSVReaderHeaderAwareBuilder(new InputStreamReader(input)).withCSVParser(parser).build()) {
+            Map<String, String> row;
+
+            while ((row = reader.readMap()) != null) {
+                allRows.add(row);
+            }
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+        }
+        return allRows;
+    }
+}
