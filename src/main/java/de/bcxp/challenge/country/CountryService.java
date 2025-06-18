bc3c27572725d2452d3376fabd445ae05f9dd1b2
Conrad Dang <conrad.dang@tum.de>
2025-06-18 15:37:46 +0200

    added CountryService class

--- src/main/java/de/bcxp/challenge/country/CountryService.java
+++ src/main/java/de/bcxp/challenge/country/CountryService.java
@@ -1,4 +1,12 @@
 package de.bcxp.challenge.country;
 
+import java.util.Comparator;
+import java.util.List;
+
 public class CountryService {
+    public CountryData findCountryWithHighestPopulationDensity(List<CountryData> dataEntries) {
+        return dataEntries.stream()
+                .max(Comparator.comparingDouble(CountryData::getPopulationDensity))
+                .orElse(null);
+    }
 }
