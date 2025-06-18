87bc213ae297f0a91ea47a3c0df5f760e681ff68
Conrad Dang <conrad.dang@tum.de>
2025-06-18 14:09:53 +0200

    implemented WeatherTask class

--- src/main/java/de/bcxp/challenge/weather/WeatherTask.java
+++ src/main/java/de/bcxp/challenge/weather/WeatherTask.java
@@ -1,4 +1,17 @@
 package de.bcxp.challenge.weather;
 
+import java.util.List;
+
 public class WeatherTask {
+    private final static String RESOURCE_PATH = "de/bcxp/challenge/weather.csv";
+    private final WeatherDataLoader weatherDataLoader = new WeatherDataLoader();
+    private final WeatherService weatherService = new WeatherService();
+
+    public String execute() {
+        List<WeatherData> dataEntries =  weatherDataLoader.loadData(RESOURCE_PATH);
+        for (WeatherData dataEntry : dataEntries) {
+            System.out.println("Day: " + dataEntry.getDay() + " MaxT: " + dataEntry.getMaxTemp() + " MinT: " + dataEntry.getMinTemp() + " Spread: " + dataEntry.getTempSpread());
+        }
+        return weatherService.findDayWithSmallestTempSpread(dataEntries).getDay();
+    }
 }
