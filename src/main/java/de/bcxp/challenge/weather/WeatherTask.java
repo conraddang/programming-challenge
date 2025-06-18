e8270767f460dbdcc3d9400aa505384083d96f5a
Conrad Dang <conrad.dang@tum.de>
2025-06-18 14:29:58 +0200

    extended with Task interface

--- src/main/java/de/bcxp/challenge/weather/WeatherTask.java
+++ src/main/java/de/bcxp/challenge/weather/WeatherTask.java
@@ -1,17 +1,20 @@
 package de.bcxp.challenge.weather;
 
+import de.bcxp.challenge.common.Task;
+
 import java.util.List;
 
-public class WeatherTask {
+public class WeatherTask implements Task {
     private final static String RESOURCE_PATH = "de/bcxp/challenge/weather.csv";
     private final WeatherDataLoader weatherDataLoader = new WeatherDataLoader();
     private final WeatherService weatherService = new WeatherService();
 
-    public String execute() {
-        List<WeatherData> dataEntries =  weatherDataLoader.loadData(RESOURCE_PATH);
-        for (WeatherData dataEntry : dataEntries) {
-            System.out.println("Day: " + dataEntry.getDay() + " MaxT: " + dataEntry.getMaxTemp() + " MinT: " + dataEntry.getMinTemp() + " Spread: " + dataEntry.getTempSpread());
-        }
+    @Override
+    public String execute() throws Exception {
+        List<WeatherData> dataEntries = weatherDataLoader.loadData(RESOURCE_PATH);
+//        for (WeatherData dataEntry : dataEntries) {
+//            System.out.println("Day: " + dataEntry.getDay() + " MaxT: " + dataEntry.getMaxTemp() + " MinT: " + dataEntry.getMinTemp() + " Spread: " + dataEntry.getTempSpread());
+//        }
         return weatherService.findDayWithSmallestTempSpread(dataEntries).getDay();
     }
 }
